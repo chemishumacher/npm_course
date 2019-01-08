a606c1cd7877a50237bad3139904d45310060731
Chemi Shumacher <chshum@microsoft.com>
2019-01-08 15:36:55 +0200

    test: added first index UT

---
+++ src/index.spec.js
@@ -0,0 +1,22 @@
+const index = require('./index.js');
+const slugger = index.slugger
+
+/**
+ * @describe [optional] - group of tests with a header to describe them
+ */
+describe('testing slugger basic functionality', () => {
+    /**
+     * @it - unit tests can use the 'it' syntax
+     */
+    it('slugger can slug string with spaces', () => {
+        const result = slugger("hey", "yo  ");
+        expect(result).toEqual("hey-yo  ");
+    })
+    /**
+     * @test - unit test can use the 'test' syntax
+     */
+    test('slugger can slug any number of spacy strings', () => {
+        const result = slugger("hey" , "yo")
+        expect(result).toEqual("hey-yo");
+    })
+})
